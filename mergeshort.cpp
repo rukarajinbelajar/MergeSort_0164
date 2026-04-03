2192ce47817beb6585f9bf2b8f4a998bc750ae89
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-03 14:34:17 +0700

    menentukan indeks

--- mergeshort.cpp
+++ mergeshort.cpp
@@ -47,3 +47,8 @@ void mergeSort(int low, int high)
     // fungsi rekursi - memanggil diri sendiri
     mergeSort(low, mid);       // step 3.a
     mergeSort(mid + 1, high);  // step 3.b
+
+    // step 4
+    int i = low;       // step 4.a
+    int j = mid + 1;   // step 4.b
+    int k = low;       // step 4.c
