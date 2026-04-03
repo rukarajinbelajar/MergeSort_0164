650f307fcd0bd40eff3cdca2a666b23ddb46e747
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-03 14:28:26 +0700

    variabel jumlah data, untuk menginput data yang akan digunakan

--- mergeshort.cpp
+++ mergeshort.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 
 //create main array&temporary array
-#include <iostream>
-//n is array input size
-using namespace std;
+int arr[20], B[20];
+// n is array input size
+int n;
+
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+}
